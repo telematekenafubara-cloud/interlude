bfb7a9dac45c54d5345471ade6ec7998bc1b129d
telematekenafubara-cloud <telematekenafubara@gmail.com>
2026-09-25 15:58:40 +0100

    Replace PLACEHOLDER with impressions.server barrel

--- src/lib/impressions.server.ts
+++ src/lib/impressions.server.ts
@@ -1,1 +1,11 @@
-PLACEHOLDER
+/**
+ * Server-side Hold impression ledger (barrel).
+ */
+export * from "./impressions-core.server";
+export {
+  recordImpression,
+  holderCounts,
+  type RecordInput,
+  type RecordResult,
+  type HolderCounts,
+} from "./impressions-record.server";
